213129dab3163bc5ae720390678f614e9c582455
Anastasia <anast111antonova@gmail.com>
2025-12-05 14:22:48 +0200

    Add app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,137 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Component } from 'react'
+import Header from './components/Header/Header'
+import Main from './components/Main/Main'
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      contacts: [],
+      currentContact: null,
+      form: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+      }
+    }
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  componentDidMount() {
+    const saved = JSON.parse(localStorage.getItem('contacts'))
+    if (saved) this.setState({contacts: saved})
+  }
+
+  saveToLocalStorage(list) {
+    localStorage.setItem('contacts', JSON.stringify(list))
+  }
+
+  handleSelectContact = (contact) => {
+    this.setState({
+      currentContact: contact,
+      form: {
+        firstName: contact.firstName,
+        lastName: contact.lastName,
+        email: contact.email,
+        phone: contact.phone,
+      }
+    })
+  }
+
+  handleFormChange = (field, value) => {
+    this.setState({form: {...this.state.form, [field]: value}})
+  }
+
+  clearField = (field) => {
+    this.setState({form: {...this.state.form, [field]: ''}})
+  }
+
+  handleNew = () => {
+    this.setState({
+      currentContact: null,
+      form: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+      }
+    })
+  }
+
+  handleSave = () => {
+    const {currentContact, contacts, form} = this.state
+
+    if (!currentContact) {
+      const newContact = {
+        id: Date.now(),
+        ...form,
+      }
+
+      const updated = [...contacts, newContact]
+      this.setState({
+        contacts: updated,
+        form: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+      }
+      })
+      this.saveToLocalStorage(updated)
+      return
+    }
+
+    const updated = contacts.map((c) => 
+      c.id === currentContact.id ? {...currentContact, ...form} : c
+    )
+
+    this.setState({contacts: updated})
+    this.saveToLocalStorage(updated)
+  }
+
+  handleDeleteCurrentContact = () => {
+    const {currentContact, contacts} = this.state
+    if (!currentContact) return
+
+    const updated = contacts.filter((c) => c.id !== currentContact.id)
+    this.setState({
+      contacts: updated,
+      currentContact: null,
+      form: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+      }
+    })
+    this.saveToLocalStorage(updated)
+  }
+
+  deleteContact = (id) => {
+    const updated = this.state.contacts.filter((c) => c.id !== id)
+    this.setState({contacts: updated})
+    this.saveToLocalStorage(updated)
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <Header />
+        <Main
+          contacts={this.state.contacts}
+          currentContact={this.state.currentContact}
+          form={this.state.form}
+          onSelectContact={this.onSelectContact}
+          onFormChange={this.onFormChange}
+          clearField={this.clearField}
+          onNew={this.handleNew}
+          onSave={this.handleSave}
+          onDelete={this.handleDeleteCurrentContact}
+          deleteContact={this.deleteContact}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
+  }
 }
 
 export default App
