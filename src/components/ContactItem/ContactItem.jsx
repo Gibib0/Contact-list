29a3dab5762fbd3b86e148480cd2989e5ca5d8b5
Anastasia <anast111antonova@gmail.com>
2025-12-09 01:04:36 +0200

    Add contactItem

---
+++ src/components/ContactItem/ContactItem.jsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+import './ContactItem.css'
+
+class ContactItem extends Component {
+	render() {
+		const {contact} = this.props
+		const {firstName, lastName} = contact
+
+		return (
+			<div className="contact-item" onDoubleClick={this.handleDoubleCLick}>
+				<span className="contact-name">{firstName} {lastName}</span>
+				<button className="contact-delete-btn" onClick={this.handleDeleteClick}>x</button>
+			</div>
+		)
+	}
+
+	handleDoubleCLick = () => {
+		this.props.onSelect(this.props.contact)
+	}
+
+	handleDeleteClick = (e) => {
+		e.stopPropagation()
+		this.props.onDelete(this.props.contact.id)
+	}
+}
+
+export default ContactItem
